14b09f4b740fed12523354b8495450f2295da28f
Vincent-Lalo <vincent.lalo.pro@gmail.com>
2023-07-04 14:30:30 +0200

    feat: move files + générate automated models + get categories table

--- Isitech-EntityFramework-1/Northwind.cs
+++ Isitech-EntityFramework-1/Northwind.cs
@@ -1,12 +1,14 @@
+namespace EFCore.Shared;
 using Microsoft.EntityFrameworkCore;
 using static System.Console;
-namespace EFCore.Shared;
+
 public class Northwind : DbContext
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         String path = Path.Combine(Environment.CurrentDirectory, "Northwind.db");
         
+        Console.WriteLine(path);
         String connection = $"Filename={path}";
         
         ConsoleColor color = Console.ForegroundColor;
@@ -20,9 +22,20 @@ public class Northwind : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         //Avec Fluent API on peut définir des règles de validation
-        /*modelBuilder.Entity<Category>()
+        modelBuilder.Entity<Category>()
             .Property(c => c.CategoryName)
             .IsRequired()
-            .HasMaxLength(15);*/
+            .HasMaxLength(15);
+        
+        if(Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite") 
+        {
+            //Règles de validations spécifiques à SQLite
+            modelBuilder.Entity<Product>()
+                .Property(p => p.UnitPrice)
+                .HasConversion<double>();
+        }
     }
+    
+    public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<Product>? Products { get; set; }
 }
