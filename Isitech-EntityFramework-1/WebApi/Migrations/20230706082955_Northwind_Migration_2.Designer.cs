fa0f010311777e99fbcce0e05cb5e5aa84aa0718
Vincent-Lalo <vincent.lalo.pro@gmail.com>
2023-07-06 10:50:48 +0200

    feat: second migration modification Product class

--- Isitech-EntityFramework-1/WebApi/Migrations/20230706082955_Northwind_Migration_2.Designer.cs
+++ Isitech-EntityFramework-1/WebApi/Migrations/20230706082955_Northwind_Migration_2.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Northwind.DBContext;
 
@@ -10,9 +11,11 @@ using Northwind.DBContext;
 namespace webapi.Migrations
 {
     [DbContext(typeof(NorthwindContext))]
-    partial class NorthwindContextModelSnapshot : ModelSnapshot
+    [Migration("20230706082955_Northwind_Migration_2")]
+    partial class Northwind_Migration_2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.8");
--- Isitech-EntityFramework-1/WebApi/Models/Product.cs
+++ Isitech-EntityFramework-1/WebApi/Models/Product.cs
@@ -25,6 +25,8 @@ public partial class Product
 
   [Column(TypeName = "numeric(18, 0)")]
   public decimal? UnitPrice { get; set; }
+  
+  public string? WareHouseName { get; set; }
 
   [NotMapped]
   [ForeignKey("CategoryId")]
